379ef65ea19f92e7277f47a38d7990d6d2dd9f84
oyaryo <file_604@yahoo.co.jp>
2020-05-09 23:37:42 +0900

    delete

--- .ipynb_checkpoints/get_price_dataframe-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import sqlite3
-
-def get_price_dataframe(db_file_name, code):
-    conn = sqlite3.connect(db_file_name)
-    return pd.read_sql('SELECT date, open, high, low, close, volume FROM prices WHERE code = ? ORDER BY date', conn, params=(code,), parse_dates=('date',), index_col='date')
